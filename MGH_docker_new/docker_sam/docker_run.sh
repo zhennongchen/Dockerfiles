89a39a106f3f2aba11cff7bb771963bdc5ad806c
Zhennong Chen <chenzhennong@gmail.com>
2024-03-18 23:33:59 -0400

    sam

--- MGH_docker_new/docker_sam/docker_run.sh
+++ MGH_docker_new/docker_sam/docker_run.sh
@@ -14,10 +14,10 @@ HOST_USER_NAME=${USER}
 CONTAINER_WORKDIR="/workspace/Documents"   #/workspace/CTProjector" 
 CONTAINER_DATADIR="/mnt" #should be /mnt/data when using NAS drive   
 
-sudo docker run -it --gpus=all --name=docker_ex --network="bridge" \
+sudo docker run -it --gpus=all --name=docker_ex --network="bridge" --shm-size=256m --ipc=host \
 -v ${HOST_WORK_DIR}:${CONTAINER_WORKDIR} \
 -v ${HOST_DATA_DIR}:${CONTAINER_DATADIR} \
--p 8120:8120 \
+-p 8160:8160 \
 -e CONTAINER_UID=${HOST_USER_ID} \
 -e CONTAINER_GID=${HOST_GROUP_ID} \
 -e CONTAINER_UNAME=${HOST_USER_NAME} \
